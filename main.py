45810ac9e4d53429905009ce6ca5623dde03ca38
Lucas Pires <69293694+luscas20@users.noreply.github.com>
2021-08-28 14:01:07 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# utilizando STRIP.
+
+frase1 = "Eu amo comer amoras no café da manhã"
+lista_termos1 = frase1.split()
+print(lista_termos1)
+
+frase2 = "Amora Abacaxi abacate banana "
+lista_termos2 = frase2.split()
+print(lista_termos2)
+
+frase3 = "carro,moto,avião"
+lista_termos3 = frase3.split(',')
+print(lista_termos3)
