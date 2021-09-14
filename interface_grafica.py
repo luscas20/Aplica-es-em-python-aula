67eaf2cb02615689c9832cca528caf13e79e88ea
Lucas Pires <69293694+luscas20@users.noreply.github.com>
2021-09-14 17:01:02 -0300

    Update interface_grafica.py

--- interface_grafica.py
+++ interface_grafica.py
@@ -22,20 +22,33 @@
 #ttk.Label(janela, text="Componente Label").grid(column=0, row=0)
 #janela.mainloop()
 
-#Usando  widget Button#
+#Usando widget Button + Entry#
+
+#import tkinter as tk
+#def mostrar_nomes():
+ #   print("nome: %s\nSobrenome: %s" % (e1.get(), e2.get()))
+#janela = tk.Tk()
+#janela.title("Aplicação GUI com o Widget Entry")
+#tk.Label(janela,text="Nome").grid(row=0)
+#tk.Label(janela,text="Sobrenome").grid(row=1)
+#e1 = tk.Entry(janela)
+#e2 = tk.Entry(janela)
+#e1.grid(row=0, column=1)
+#e2.grid(row=1, column=1)
+#tk.Button(janela, text='sair', 
+#command=janela.quit).grid(row=3,column=0,sticky=tk.W,pady=4)
+#tk.Button(janela, text='exibir Dados', command=mostrar_nomes).grid(row=3,column=1, sticky=tk.W,pady=4)
+#tk.mainloop()
+
+# Radio Buttons#
 
 import tkinter as tk
-def mostrar_nomes():
-    print("nome: %s\nSobrenome: %s" % (e1.get(), e2.get()))
-janela = tk.Tk()
-janela.title("Aplicação GUI com o Widget Entry")
-tk.Label(janela,text="Nome").grid(row=0)
-tk.Label(janela,text="Sobrenome").grid(row=1)
-e1 = tk.Entry(janela)
-e2 = tk.Entry(janela)
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
-tk.Button(janela, text='sair', 
-command=janela.quit).grid(row=3,column=0,sticky=tk.W,pady=4)
-tk.Button(janela, text='exibir Dados', command=mostrar_nomes).grid(row=3,column=1, sticky=tk.W,pady=4)
-tk.mainloop()
+janela =  tk.Tk()
+v = tk.IntVar()
+tk.Label(janela, text="Escolha uma linguagem de programação", justify = tk.LEFT,padx=20).pack()
+tk.Radiobutton(janela, text="python",
+padx=25,variable=v,value=1).pack(anchor=tk.W)
+tk.Radiobutton(janela, text="C++", padx=25,variable=v,value=2).pack(anchor=tk.W)
+tk.Radiobutton(janela, text="HTML5", padx=25,variable=v,value=3).pack(anchor=tk.W)
+tk
+janela.mainloop()
